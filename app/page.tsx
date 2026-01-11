c3905ff498161650d05cb25fd866318e7533fcee
marufkhan20 <mdm326040@gmail.com>
2026-01-11 13:36:37 +0600

    Implement hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/landing/hero";
 import Navbar from "@/components/landing/nav";
 
 const Home = () => {
   return (
     <main className="w-full flex flex-col relative z-10">
       <Navbar />
+      <Hero />
     </main>
   );
 };
